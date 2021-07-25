c62da5baed7f783bb6e20da1f710c80eba28042a
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-07-25 15:13:47 +0100

    adds layout styling to result page

--- src/pages/ResultPage/ResultPageContainer.js
+++ src/pages/ResultPage/ResultPageContainer.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 import RouteButton from '../../common/containers/RouteButton';
 
+import { Body, Footer } from '../../common/components';
+import NavBar from './../../common/containers/NavBar';
+
 const ResultPageContainer = () => {
   return (
-    <div>
-      <h1>Result Page</h1>
-      <RouteButton route="">Go Home</RouteButton>
-      <RouteButton route="dashboard">View Dashboard</RouteButton>
-    </div>
+    <>
+      <NavBar score />
+      <Body>
+        <h1>Well Done!</h1>
+        <p>Today you gained x amounts of stars. </p>
+      </Body>
+      <Footer>
+        <RouteButton route="dashboard">View Dashboard</RouteButton>
+      </Footer>
+    </>
   );
 };
 
