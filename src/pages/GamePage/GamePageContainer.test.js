f8f3eabe2c721b623002d008bd8f648a2692170e
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-08-16 13:14:36 +0100

    adds test to GamePageContainer

--- src/pages/GamePage/GamePageContainer.test.js
+++ src/pages/GamePage/GamePageContainer.test.js
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { storeFactory } from '../../utils/testUtils';
 
 import GamePageContainer from './GamePageContainer';
-import App from './../../App';
 
 describe('GamePageContainer Component', () => {
   it('should render correctly', () => {
