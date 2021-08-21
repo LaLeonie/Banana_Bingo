2be4d2209f50f6dcd0bab8569b71049ca754eafb
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-08-21 21:29:09 +0100

    create custom render function

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import { createStore } from 'redux';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
 
 import { rootReducer } from '../store/store';
 
-export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+const render = (
+  ui,
+  {
+    initialState,
+    store = createStore(rootReducer, initialState),
+    ...renderOptions
+  } = {}
+) => {
+  const Wrapper = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 };
+
+export * from '@testing-library/react';
+
+export { render };
