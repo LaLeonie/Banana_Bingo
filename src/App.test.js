c23b66b9cca3301c8f55bad53228861f94786957
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-08-23 17:59:01 +0100

    adds test to navigate to game paage

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { render, screen, fireEvent } from './test/testUtils';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 describe('happy path functions as expected', () => {
-  test('HomePage renders on starting app', () => {
+  test('HomePage renders on starting app with header and button', () => {
     render(<App />);
-    const header = screen.getByRole('heading', { name: 'Banana Bingo' });
-    expect(header).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: 'Banana Bingo' })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Stats' })).toBeInTheDocument();
+  });
+
+  test('I can navigate to GamePage', () => {
+    render(<App />);
+    userEvent.click(screen.getByText(/play/i));
+
+    expect(screen.getByText(/Loading/)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: "I'm Done" })
+    ).toBeInTheDocument();
   });
 });
