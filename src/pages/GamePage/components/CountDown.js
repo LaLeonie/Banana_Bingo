980e88ef56d627c68454b691977b9073ebb4d670
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-08-28 11:16:54 +0200

    changes display of countdown carad

--- src/pages/GamePage/components/CountDown.js
+++ src/pages/GamePage/components/CountDown.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useCountdown } from '../../../hooks/useCountdown';
 
@@ -12,8 +12,7 @@ const CountdownDialog = styled.div`
   justify-content: center;
 
   .countdown-card {
-    background-color: lightgrey;
-    border: solid grey 10px;
+    background-color: #fff;
     border-radius: 4px;
     display: flex;
     align-items: center;
@@ -21,20 +20,37 @@ const CountdownDialog = styled.div`
     justify-content: center;
     height: 550px;
     width: 550px;
-    font-size: 12rem;
+    font-size: 10rem;
   }
 `;
 
 const CountDown = ({ setCountdownDisplay }) => {
+  let [displayText, setDisplayText] = useState('');
   let { display, count } = useCountdown(3);
 
   useEffect(() => {
     setCountdownDisplay(display);
   }, [setCountdownDisplay, display]);
 
+  useEffect(() => {
+    switch (count) {
+      case 3:
+        setDisplayText('Ready');
+        break;
+      case 2:
+        setDisplayText('Steady');
+        break;
+      case 1:
+        setDisplayText('GO');
+        break;
+      default:
+        setDisplayText('');
+    }
+  }, [setDisplayText, count]);
+
   return (
     <CountdownDialog>
-      <div className="countdown-card">{count}</div>
+      <div className="countdown-card">{displayText}</div>
     </CountdownDialog>
   );
 };
