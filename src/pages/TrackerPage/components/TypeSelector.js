4f09c535ebb75e29830c7c2f265f7b8948c96125
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-01 08:08:12 +0200

    adds Form.Group andd TypeSelectorWrapper

--- src/pages/TrackerPage/components/TypeSelector.js
+++ src/pages/TrackerPage/components/TypeSelector.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { InputGroup, FormControl } from 'react-bootstrap';
+import { Form, FormControl } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const TypeSelectorWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
 
 const TypeSelector = () => {
   return (
-    <>
-      <InputGroup className="mb-3">
-        <InputGroup.Checkbox aria-label="Checkbox for Fruit" />
-        <FormControl aria-label="Fruit" />
-      </InputGroup>
-      <InputGroup>
-        <InputGroup.Checkbox aria-label="Checkbox for Veg" />
-        <FormControl aria-label="Veg" />
-      </InputGroup>
-    </>
+    <TypeSelectorWrapper>
+      <Form.Group>
+        <Form.Check checked type="checkbox" label="Fruit" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Check checked type="checkbox" label="Veg" />
+      </Form.Group>
+    </TypeSelectorWrapper>
   );
 };
 
