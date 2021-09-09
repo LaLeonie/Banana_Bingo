9bd89422324551168d324842f8b6f665af7add42
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-09 15:00:06 +0200

    adds test setup for GamePageContainer

--- src/pages/GamePage/GamePageContainer.test.js
+++ src/pages/GamePage/GamePageContainer.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { customRender } from '../../test/testUtils';
-import { render } from '@testing-library/react';
-import { Provider } from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import App from '../../App';
+
 import userEvent from '@testing-library/user-event';
+
+jest.mock('../../hooks/useFetch');
+
+jest.mock('../../hooks/useRandom');
+
+beforeEach(() => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  userEvent.click(screen.getByText(/play/i));
+});
+
+describe('Testing the GamePage Container', () => {});
 
 test('text runs', () => {
   console.log('runs');
