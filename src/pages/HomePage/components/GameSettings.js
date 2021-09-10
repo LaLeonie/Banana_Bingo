46343ffd9c7ecc7b64a80a3bff374d61d7169180
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-10 21:50:12 +0200

    adds close button to GameSettings component

--- src/pages/HomePage/components/GameSettings.js
+++ src/pages/HomePage/components/GameSettings.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import CloseButton from 'react-bootstrap/CloseButton';
+
 const SettingsContainer = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
+  min-height: 40vh;
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: yellow;
+  background-color: lightgray;
   border-radius: 4px;
   border: solid black 2px;
 `;
 
-const GameSettings = () => {
+const GameSettings = ({ setShowSettings }) => {
   return (
     <SettingsContainer>
+      <CloseButton onClick={() => setShowSettings(false)} aria-label="Hide" />
       <h1>Choose the difficulty level</h1>
     </SettingsContainer>
   );
