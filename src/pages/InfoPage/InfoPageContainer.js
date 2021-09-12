c9f9410982a5712b5582b1e785b965d32d8c6a16
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-12 08:34:30 +0200

    adds text to info page

--- src/pages/InfoPage/InfoPageContainer.js
+++ src/pages/InfoPage/InfoPageContainer.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import { Body, BasicTitle } from '../../common/components';
 import RouteButton from '../../common/containers/RouteButton';
 
 const InfoPageContainer = () => {
   return (
-    <div>
-      <h1>Info Page</h1>
+    <Body>
+      <BasicTitle>How To Play?</BasicTitle>
+      <p>
+        Banana Bingo is as easy as standard Bingo. You win if you strike 5
+        plants in a row - horizontally, vertically and diagonally. You may only
+        select plants that you've eaten today. So to win, you must have had your
+        5 A Day! Be quick, the timer is running. And beware, you can only play
+        once a day!
+      </p>
+      <ul>
+        <li>For a Bingo, you gain 10 stars</li>
+        <li>For every plant you ate, you gain an extra stars</li>
+      </ul>
+      <p>
+        So the more plants you eat in a day, the higher your score. Don't worry
+        if you don't find all your plants first, you can add extra plants after
+        the game
+      </p>
       <RouteButton back>Go Back</RouteButton>
-    </div>
+    </Body>
   );
 };
 
