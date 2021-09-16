6dca4ac85646c63a7778ecd0686f6f148eaa992b
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-16 22:15:10 +0200

    adds conditoin to BasicButton

--- src/common/components/Buttons.js
+++ src/common/components/Buttons.js
@@ -6,7 +6,9 @@ const BasicButton = styled(Button)`
   appearance: none;
   background-image: radial-gradient(
     100% 100% at 100% 0,
-    ${({ theme: { colors } }) => colors.primaries.blue.main} 0,
+    ${({ primary, theme: { colors } }) =>
+        primary ? colors.primaries.blue.main : colors.neutrals[50]}
+      0,
     ${({ theme: { colors } }) => colors.primaries.blue.dark} 100%
   );
   border: 0;
@@ -21,6 +23,7 @@ const BasicButton = styled(Button)`
   font-family: ${({ theme: { fonts } }) => fonts.regular[0]};
   height: 52px;
   justify-content: center;
+  letter-spacing: 0.1rem;
   line-height: 1;
   list-style: none;
   min-width: 160px;
@@ -37,7 +40,29 @@ const BasicButton = styled(Button)`
   touch-action: manipulation;
   white-space: nowrap;
   will-change: box-shadow, transform;
-  font-size: 18px;
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.regular};
+  font-weight: 700;
+
+  :focus {
+    box-shadow: ${({ theme: { colors } }) => colors.primaries.blue.dark} 0 0 0
+        1.5px inset,
+      ${({ theme: { colors } }) => colors.neutrals[500]} 0 2px 4px,
+      ${({ theme: { colors } }) => colors.neutrals[400]}0 7px 13px -3px,
+      ${({ theme: { colors } }) => colors.primaries.blue.dark} 0 -3px 0 inset;
+  }
+
+  :hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px,
+      rgba(45, 35, 66, 0.3) 0 7px 13px -3px,
+      ${({ theme: { colors } }) => colors.primaries.blue.dark} 0 -3px 0 inset;
+    transform: translateY(-2px);
+  }
+
+  :active {
+    box-shadow: ${({ theme: { colors } }) => colors.primaries.blue.dark}0 3px
+      7px inset;
+    transform: translateY(2px);
+  }
 `;
 
 export const PrimaryButton = styled(BasicButton)``;
