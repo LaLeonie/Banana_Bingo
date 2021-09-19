747337dc55dbe6147004fddcfa61d951184f6865
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-19 20:23:59 +0100

    removes local fonts

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,22 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import Neucha from './fonts/neucha-v12-latin-regular.woff';
-import Neucha2 from './fonts/neucha-v12-latin-regular.woff2';
-import Ranchers from './fonts/ranchers-v8-latin-regular.woff';
-import Ranchers2 from './fonts/ranchers-v8-latin-regular.woff2';
 
 const GlobalStyle = createGlobalStyle`
-/* @font-face {
-    font-family: 'Neucha';
-  src: url(${Neucha2}) format('woff2'),
-       url(${Neucha}) format('woff');
-}
-
-@font-face {
-    font-family: 'Ranchers';
-    src: url(${Ranchers2}) format('woff2'),
-       url(${Ranchers}) format('woff');
-} */
-
 body{
     font-family: ${({ theme: { fonts } }) => fonts.regular[0]}; 
     font-size: ${({ theme: { fontSizes } }) => fontSizes.regularPlus}; 
