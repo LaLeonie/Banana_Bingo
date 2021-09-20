5006339463352cd4f1e69a701039bb48563c1ac6
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-09-20 09:09:43 +0100

    adds styling to ResultAlert

--- src/pages/GamePage/components/ResultAlert.js
+++ src/pages/GamePage/components/ResultAlert.js
@@ -32,14 +32,14 @@ const ResultContainer = styled.div`
         victory ? colors.primaries.green.dark : colors.primaries.red.dark}
       0px -23px 25px 0px inset,
     ${({ victory, theme: { colors } }) =>
-        victory ? colors.primaries.green.dark : colors.primaries.red.dark}
+        victory ? colors.primaries.green.main : colors.primaries.red.main}
       0px -36px 30px 0px inset,
     ${({ victory, theme: { colors } }) =>
         victory ? colors.primaries.green.main : colors.primaries.red.main}
       0px -79px 40px 0px inset,
     rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
-    rgba(0, 0, 0, 0.1) 0px 8px 4px, rgba(0, 0, 0, 0.1) 0px 16px 8px,
-    rgba(0, 0, 0, 0.1) 0px 32px 16px;
+    rgba(0, 0, 0, 0.1) 0px 8px 4px, rgba(0, 0, 0, 0.15) 0px 16px 8px,
+    rgba(0, 0, 0, 0.2) 0px 32px 16px, 2px 2px 119px 28px rgba(0, 0, 0, 0.4);
   border-radius: 150px;
   display: flex;
   flex-direction: column;
